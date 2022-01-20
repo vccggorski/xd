261714816d135d7c174a6b04d48f75414c509366
Gabriel Górski <gabriel.gorski@volvocars.com>
2022-01-20 21:48:22 +0100

    lol

--- compare.fish
+++ compare.fish
@@ -2,9 +2,9 @@
 
 set DECOMPRESSOR target/release/xd
 set INPUT /usr/bin/docker
-set OUTPUT /tmp/docker
+set OUTPUT /tmp/output
 
-lzma -c $INPUT | $DECOMPRESSOR > $OUTPUT && \
+lzma -c --lzma1=dict=4KiB $INPUT | $DECOMPRESSOR > $OUTPUT && \
 cat \
     (du -b $INPUT $OUTPUT | psub) \
     (diff (hexdump -C $INPUT | psub) (hexdump -C $OUTPUT | psub) | psub) \
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use env_logger;
 #[cfg(feature = "blocking")]
 fn main() {
+    env_logger::init();
     use lzma_rs::*;
     lzma_decompress(
         &mut std::io::BufReader::new(std::io::stdin()),
@@ -10,6 +12,7 @@ fn main() {
 
 #[cfg(feature = "blocking-no-std")]
 fn main() {
+    env_logger::init();
     use lzma_rs::*;
     let mut memory = vec![0; 9_000_000];
     let mm = allocator::MemoryDispenser::new(&mut memory);
@@ -25,24 +28,30 @@ fn main() {
 
 #[cfg(feature = "stream")]
 fn main() {
+    env_logger::init();
     use lzma_rs::decompress::*;
+    let mut stream = Stream::new(std::io::stdout());
     std::io::copy(
         &mut std::io::BufReader::new(std::io::stdin()),
-        &mut Stream::new(std::io::stdout()),
+        &mut stream,
     )
     .unwrap();
+    stream.finish().unwrap();
 }
 
 #[cfg(feature = "stream-no-std")]
 fn main() {
-    use lzma_rs::*;
+    env_logger::init();
     use lzma_rs::decompress::*;
+    use lzma_rs::*;
     let mut memory = vec![0; 9_000_000];
     let mm = allocator::MemoryDispenser::new(&mut memory);
+    let mut stream = Stream::no_std_new(&mm, std::io::stdout());
     std::io::copy(
         &mut std::io::BufReader::new(std::io::stdin()),
-        &mut Stream::no_std_new(&mm, std::io::stdout()),
+        &mut stream,
     )
     .unwrap();
+    stream.finish().unwrap();
     eprintln!("Allocator state: {:#?}", mm);
 }
