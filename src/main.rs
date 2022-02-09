67e1ec101341e0bc4f368f3838b36cdd05275284
Gabriel Górski <gabriel.gorski@volvocars.com>
2022-02-09 20:38:53 +0100

    Compressed data over serial, reading back uncompressed data

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,41 @@
+use core::time::Duration;
 use env_logger;
-#[cfg(feature = "blocking")]
+use serialport;
+use serialport::SerialPort;
+use std::thread;
 fn main() {
     env_logger::init();
-    use lzma_rs::*;
-    lzma_decompress(
-        &mut std::io::BufReader::new(std::io::stdin()),
-        &mut std::io::stdout(),
-    )
-    .unwrap();
-}
 
-#[cfg(feature = "blocking-no-std")]
-fn main() {
-    env_logger::init();
-    use lzma_rs::*;
-    let mut memory = vec![0; 9_000_000];
-    let mm = allocator::MemoryDispenser::new(&mut memory);
-    no_std_lzma_decompress_with_options(
-        &mm,
-        &mut std::io::BufReader::new(std::io::stdin()),
-        &mut std::io::stdout(),
-        &Default::default(),
-    )
-    .unwrap();
-    eprintln!("Allocator state: {:#?}", mm);
-}
-
-#[cfg(feature = "stream")]
-fn main() {
-    env_logger::init();
-    use lzma_rs::decompress::*;
     use std::io::Read;
     use std::io::Write;
-    let mut stream = Stream::new(std::io::stdout());
-    let mut bytes = [0_u8; 1 << 22];
-    let mut source = std::io::BufReader::new(std::io::stdin());
-    loop {
-        let bytes_read = source.read(&mut bytes).unwrap();
-        if bytes_read == 0 {
-            break
+    let mut input = std::io::stdin();
+    let mut output = std::io::stdout();
+    let mut serial1 = serialport::new("/dev/zvitek/ttyUART1", 30_000)
+        .open_native()
+        .unwrap();
+    let mut serial2 = serialport::new("/dev/zvitek/ttyUART2", 4_000_000)
+        .open_native()
+        .unwrap();
+    let _ = thread::spawn(move || loop {
+        let mut bytes = [0_u8; 128];
+        match input.read(&mut bytes) {
+            Ok(bytes_read) => {
+                serial1.write_all(&bytes[..bytes_read]).unwrap();
+                serial1.flush().unwrap();
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e),
         }
-        stream.write_all(&bytes[..bytes_read]).unwrap();
-        let status = stream.get_stream_status();
-        eprintln!("{:?}", &status);
-        if let StreamStatus::Finished = status {
-            break;
+    });
+    let mut bytes = [0_u8; 128];
+    loop {
+        match serial2.read(&mut bytes) {
+            Ok(bytes_read) => {
+                output.write_all(&bytes[..bytes_read]).unwrap();
+                output.flush().unwrap();
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e),
         }
     }
-    stream.finish().unwrap();
-}
-
-#[cfg(feature = "stream-no-std")]
-fn main() {
-    env_logger::init();
-    use lzma_rs::decompress::*;
-    use lzma_rs::*;
-    use std::io::Read;
-    use std::io::Write;
-    let mut memory = vec![0; 9_000_000];
-    let mm = allocator::MemoryDispenser::new(&mut memory);
-    let mut stream = Stream::no_std_new(&mm, std::io::stdout());
-    let mut bytes = [0_u8; 1 << 22];
-    let mut source = std::io::BufReader::new(std::io::stdin());
-    loop {
-        let bytes_read = source.read(&mut bytes).unwrap();
-        if bytes_read == 0 {
-            break
-        }
-        stream.write_all(&bytes[..bytes_read]).unwrap();
-        let status = stream.get_stream_status();
-        eprintln!("{:?}", &status);
-        if let StreamStatus::Finished = status {
-            break;
-        }
-    }
-    stream.finish().unwrap();
-    eprintln!("Allocator state: {:#?}", mm);
 }
